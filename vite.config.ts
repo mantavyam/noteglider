72bb15e7992a6425711781fb678f78beb8f7d779
Shivam <146970003+mantavyam@users.noreply.github.com>
2025-05-09 22:23:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "bc21abf6-8dd5-472c-858f-3b5b2dd95489.lovableproject.com",
+    ],
   },
   plugins: [
     react(),
